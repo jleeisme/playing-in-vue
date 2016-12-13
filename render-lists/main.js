2a8543abb7a6b67f4a5f45f70bf407cb1b2b9032
Jason Lee <jleeisme@gmail.com>
2016-12-12 16:30:42 -0800

    working yet with a warning

--- render-lists/main.js
+++ render-lists/main.js
@@ -1,11 +1,39 @@
 Vue.component('tasks', {
   props: ['list'],
   template: '#tasks-template',
+  // having issue with creating a local mutable copy of the prop
+  // wa wa wa
+  data: function() {
+    return { 
+      list: this.list1
+    }
+  },
+  computed: {
+    remaining: function() {
+      return this.list.filter(this.inProgress).length;
+    }
+    // initialList: function() {
+    //   return JSON.parse(this.list);
+    // }
+  },
   methods: {
     toggleCompleted: function(task) {
       task.completed = !task.completed;
+    },
+    isComplete: function(task) {
+      return task.completed;
+    },
+    inProgress: function(task) {
+      return !this.isComplete(task);
+    },
+    deleteTask: function(task) {
+      this.list.splice(this.list.indexOf(task), 1);
+    },
+    clearCompleted: function(task) {
+      this.list = this.list.filter(this.inProgress);
     }
   }
+  
 });
 
 new Vue({
