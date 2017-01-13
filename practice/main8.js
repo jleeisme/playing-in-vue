18a0c373e63a60bab52bfc94553d2e69558ec16b
Jason Lee <jleeisme@gmail.com>
2017-01-12 17:54:49 -0800

    Messing around

--- practice/main8.js
+++ practice/main8.js
@@ -59,6 +59,32 @@ Vue.component('tab', {
   }
 });
 
+Vue.component('something', {
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        
+        <div class="box">
+          <slot></slot>
+        </div>
+
+      </div> 
+    <button class="modal-close" @click="$emit('close')"></button>
+  </div>
+  `,
+});
+
+new Vue({
+  el: '#other',
+
+  data: {
+    modalVisible: false
+  }
+});
+
 new Vue({
   el: '#root'
+
+
 });
