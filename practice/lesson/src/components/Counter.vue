01f2007674ac6c1bcd76d5fcc2d947695dfc6171
Jason Lee <jleeisme@gmail.com>
2017-01-17 15:58:34 -0800

    hot modules very hot

---
+++ practice/lesson/src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>count is: {{ count }}</h2>
+    <p>
+      <button @click="count += 3">increment</button>
+      <button @click="count -= 3">decrement</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return { count: 0 }; 
+    }
+  }
+</script>
