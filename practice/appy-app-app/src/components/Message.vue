3440375766d0e0b258df3b734c02318158638fbe
Jason Lee <jleeisme@gmail.com>
2017-01-16 23:04:03 -0800

    cli webpack
    basic import export of components

---
+++ practice/appy-app-app/src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box is-info">
+    <h1>
+      <slot></slot>
+    </h1>
+  </div>  
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  .box {
+    background: pink;
+    padding: 10px;
+    border: 1px solid cyan; 
+  }
+</style>
